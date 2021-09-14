43dc654fa2b0937d7af393e8e32fa79d3502c178
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-09-14 00:14:56 -0300

    Exercício 5: vsoma valores dos três ângulos internos de um triângulo

---
+++ fundamementos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript-e-primeiros passos/ex-05.js
@@ -0,0 +1,25 @@
+/*
+5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+Um ângulo será considerado inválido se não tiver um valor positivo.
+
+Saber mais: https://blogdoenem.com.br/triangulos-propriedades/
+*/
+
+let degreeAngleA = 65;
+let degreeAngleB = 100;
+let degreeAngleC = 15;
+
+let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+
+if(allAnglesArePositives){
+  if (sumOfAngles === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  };
+} else {
+  console.log('Erro: ângulo inválido');
+}
