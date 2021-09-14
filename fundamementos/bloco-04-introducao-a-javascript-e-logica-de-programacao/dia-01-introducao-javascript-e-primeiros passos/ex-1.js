a47709cb5758eec9c0177fcfe0adff1c6d113335
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-09-13 21:48:39 -0300

    Adiciona exercício 1: AOperadores Aritméticos e Adiciona novas pastas e readme.md

---
+++ fundamementos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript-e-primeiros passos/ex-1.js
@@ -0,0 +1,34 @@
+/*
+1. Faça cinco programas, um para cada operação aritmética básica.
+Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados.
+Faça programas para:
+
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)
+*/
+
+let num1 = 10;
+let num2 = 5;
+
+//Soma - sum
+let sum = (num1 + num2);
+console.log('A soma é: ' + sum);
+
+//Subtração - subtraction
+let subtraction = (num1 - num2);
+console.log('A subtração é: ' + subtraction);
+
+//Multiplicação - multiplication
+let multiplication = num1 * num2;
+console.log('A multiplicação é: ' + multiplication);
+
+//Divisão - division
+let division = num1 / num2;
+console.log('A divisão é: ' + division);
+
+//Módulo - module => Resto da divisão
+let modulo = (num1 % num2);
+console.log('O módulo ou resto da divisão é: ' + modulo);
