7b73d45396fb8c292d4a18451b238227fb3f071e
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-09-20 19:47:52 -0300

    Alterar nome de arquivos

---
+++ fundamementos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmo/ex-02-triangulo-asteriscos.js
@@ -0,0 +1,17 @@
+//Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
+
+// n = 5
+// *
+// **
+// ***
+// ****
+// *****
+
+let size = 5;
+let symbol = '*';
+let inputLine= '';
+
+for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+  console.log(inputLine);
+  inputLine = inputLine + symbol;
+};
