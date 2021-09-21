4ec05d8ebd883aca5588834eb04877f414808bf5
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-09-21 15:15:53 -0300

    Exercício 6: Números Primos

---
+++ fundamementos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmo/ex-06-numeros-primos.js
@@ -0,0 +1,13 @@
+//Exercício 6
+//Faça um programa que diz se um número definido numa variável é primo ou não.
+//Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+
+let divisors = 1;
+let numberToCheck = 30;
+
+for (let number = 2; number <= numberToCheck; number += 1) {
+  if (numberToCheck % number === 0) divisors += 1;
+}
+
+if (divisors === 2) console.log(numberToCheck + ' é primo!');
+else console.log(numberToCheck + ' não é primo!');
