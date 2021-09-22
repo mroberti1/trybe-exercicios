5a21876b406af1eeb15281e554d9503d8e40aebb
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-09-22 00:50:27 -0300

    Adiciona Ex 3: Funções

---
+++ fundamementos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes.js/ex-03-funcoes.js
@@ -0,0 +1,12 @@
+/*
+Funções
+Funções são blocos de códigos que encapsulam instruções que executam uma tarefa específica
+Visam modularizar nossos programas, ou seja, dividir um programa em partes menores, em que, essas partes menores tenham uma única responsabilidade.
+*/
+
+function name(params) {
+  
+  //Código faz alguma coisa
+  
+}
+
